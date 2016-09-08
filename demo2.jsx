20dbbbea9550ecbe32746796978dc21effa69623
seven.cat <490542807@qq.com>
2016-09-09 01:09:51 +0800

    Delete demo2.jsx

--- demo2.jsx
+++
@@ -1,9 +0,0 @@
-    var HW = React.createClass({
-        render: function() {
-            return React.DOM.h1(null, this.props.name);
-        }
-    });
-    ReactDOM.render(
-           <HW name='helloworld'/>,
-            document.getElementById('container')
-    );
