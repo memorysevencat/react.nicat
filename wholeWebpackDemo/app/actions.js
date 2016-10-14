e2262863718f6ff2718dd6ba81925bd01634b42d
seven.cat <490542807@qq.com>
2016-10-14 11:18:20 +0800

    react-redux

--- wholeWebpackDemo/app/actions.js
+++ wholeWebpackDemo/app/actions.js
@@ -3,7 +3,7 @@
  */
 
 export const ADD_TODO = 'ADD_TODO';
-export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const COMPLETE_TODO = 'COMPLETE_TODO';
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
 
 /*
@@ -14,7 +14,7 @@ export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
   SHOW_COMPLETED: 'SHOW_COMPLETED',
   SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
+};
 
 /*
  * action 创建函数
@@ -24,8 +24,8 @@ export function addTodo(text) {
   return { type: ADD_TODO, text }
 }
 
-export function toggleTodo(index) {
-  return { type: TOGGLE_TODO, index }
+export function completeTodo(index) {
+  return { type: COMPLETE_TODO, index }
 }
 
 export function setVisibilityFilter(filter) {
--- wholeWebpackDemo/app/reducers.js
+++ wholeWebpackDemo/app/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
+import { ADD_TODO, COMPLETE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
 const { SHOW_ALL } = VisibilityFilters
 
 function visibilityFilter(state = SHOW_ALL, action) {
@@ -21,15 +21,14 @@ function todos(state = [], action) {
           completed: false
         }
       ]
-    case TOGGLE_TODO:
-      return state.map((todo, index) => {
-        if (index === action.index) {
-          return Object.assign({}, todo, {
-            completed: !todo.completed
-          })
-        }
-        return todo
-      })
+    case COMPLETE_TODO:
+      return [
+        ...state.slice(0, action.index),
+        Object.assign({}, state[action.index], {
+          completed: true
+        }),
+        ...state.slice(action.index + 1)
+      ]
     default:
       return state
   }
